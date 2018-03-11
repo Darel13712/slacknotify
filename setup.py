44e689fe7fabc66f4517a4da6a8b19a5fcba5451
Darel <darel142857@gmail.com>
2018-03-11 18:21:10 +0300

    added url

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='slacknotify',
       version='0.1',
       description='Send notifications to slack from python',
-      url='',
+      url='https://github.com/Darel13712/slacknotify',
       author='Darel',
       author_email='darel142857@gmail.com',
       license='MIT',
